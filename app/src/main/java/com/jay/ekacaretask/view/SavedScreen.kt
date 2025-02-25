6c9f796b4417b7fc988fc4fc80ff1585ec053d5e
Jaykumar <jayvaghasiya25@gmail.com>
2025-02-25 16:24:35 +0530

    Delete Integrated

--- app/src/main/java/com/jay/ekacaretask/view/SavedScreen.kt
+++ app/src/main/java/com/jay/ekacaretask/view/SavedScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.basicMarquee
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -21,15 +20,25 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -45,6 +54,7 @@ import org.koin.androidx.compose.koinViewModel
 fun SavedScreen(navController: NavHostController) {
     val viewModel: LocalNewsViewModel = koinViewModel()
     val savedarticleState by viewModel.savedArticles.collectAsState()
+    var showDialog by remember { mutableStateOf(false) }
 
     AnimatedContent(
         targetState = savedarticleState,
@@ -55,7 +65,9 @@ fun SavedScreen(navController: NavHostController) {
         if (targetState.isNotEmpty()) {
             LazyColumn(modifier = Modifier.fillMaxSize(), contentPadding = PaddingValues(12.dp)) {
                 items(savedarticleState) { article ->
-                    ArticlesItem(article) {
+                    ArticlesItem(article, onDeleteClick = {
+                        showDialog = true
+                    }) {
                         navController.navigate(
                             Web(
                                 url = article.url,
@@ -65,8 +77,37 @@ fun SavedScreen(navController: NavHostController) {
                             )
                         )
                     }
+                    if (showDialog) {
+                        AlertDialog(
+                            onDismissRequest = { showDialog = false },
+                            title = { Text("Delete Article") },
+                            text = { Text("Are you sure you want to delete this article?") },
+                            confirmButton = {
+                                TextButton(onClick = {
+                                    viewModel.deleteArticle(
+                                        article = Articles(
+                                            article.id,
+                                            article.title,
+                                            article.description,
+                                            article.urlOfImage,
+                                            article.url
+                                        )
+                                    )
+                                    showDialog = false
+                                }) {
+                                    Text("Delete")
+                                }
+                            },
+                            dismissButton = {
+                                TextButton(onClick = { showDialog = false }) {
+                                    Text("Cancel")
+                                }
+                            }
+                        )
+                    }
                 }
             }
+
         } else {
             Box(
                 modifier = Modifier
@@ -80,7 +121,7 @@ fun SavedScreen(navController: NavHostController) {
 }
 
 @Composable
-fun ArticlesItem(article: Articles, onClick: () -> Unit) {
+fun ArticlesItem(article: Articles, onDeleteClick: () -> Unit, onClick: () -> Unit) {
     ElevatedCard(
         modifier = Modifier
             .fillMaxWidth()
@@ -130,22 +171,31 @@ fun ArticlesItem(article: Articles, onClick: () -> Unit) {
                 .fillMaxWidth()
                 .padding(8.dp)
         )
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(4.dp)
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.End
         ) {
-            Text(
-                text = "Read More",
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier
-                    .align(Alignment.CenterEnd)
-                    .padding(vertical = 8.dp, horizontal = 16.dp)
-                    .clickable {
-                        onClick()
-                    })
+
+            TextButton(
+                onClick = { onClick() },
+            ) {
+                Text(
+                    text = "Read More",
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(vertical = 8.dp, horizontal = 16.dp)
+                )
+            }
+            IconButton(onClick = { onDeleteClick() }) {
+                Icon(Icons.Default.Delete, contentDescription = null, tint = Color.Red)
+
+            }
+
+        }
+
         }
 
 
-    }
 }
